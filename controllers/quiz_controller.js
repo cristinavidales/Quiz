be84ba73d3614aa447acd94d3fbd9d0183674b32
cristinavidales <cristina.vidales.serrano@alumnos.upm.es>
2015-05-17 14:00:19 +0200

    errors[]

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,13 +13,19 @@ next();
 //POST /quizes/create
 exports.create = function(req,res){
 	var quiz = models.Quiz.build(req.body.quiz);
-//guarda en DB los campos pregunta y respusta de quiz
-quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-	res.redirect('/quizes');
 
-}) //redireccion http (URL RELATIVO) lista de preguntas
-
+	quiz.validate().then(function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta" ]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
 };
+
 
 
 // GET /quizes
@@ -35,7 +41,7 @@ res.render('quizes/index.ejs', {quizes: quizes});
 	console.log(search);
 models.Quiz.findAll({where: ["pregunta like ?", search]}).then(
 function(quizes) {
-res.render('quizes/index.ejs', {quizes: quizes});
+res.render('quizes/index.ejs', {quizes: quizes, errors: []});
 })}
 
 };
@@ -43,7 +49,7 @@ res.render('quizes/index.ejs', {quizes: quizes});
 // GET /quizes/question
 exports.show = function(req, res){
 
-res.render('quizes/show', { quiz: req.quiz});
+res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 //GET /quizes/answer
 exports.answer = function(req, res){
@@ -51,7 +57,9 @@ var resultado='Incorrecto';
 if (req.query.respuesta === req.quiz.respuesta){
 	resultado='Correcto';
 }
-res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado}); 
+res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []
+}
+); 
 
 };
 
@@ -63,6 +71,6 @@ exports.new = function(req, res){
 	var quiz = models.Quiz.build( //crea objeto quiz
 		{pregunta: "Pregunta", respuesta: "Respuesta"}
 		);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
