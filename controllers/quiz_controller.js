6696a94d2799fb3a1006e9e9d8aea110ca1a634b
cristinavidales <cristina.vidales.serrano@alumnos.upm.es>
2015-05-18 13:16:48 +0200

    borrar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -27,6 +27,12 @@ exports.create = function(req,res){
   );
 };
 
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then( function() {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
 
 
 // GET /quizes
