52d9434857b4589e5aa6c62035850ccd20fa0f4e
cristinavidales <cristina.vidales.serrano@alumnos.upm.es>
2015-05-18 15:19:35 +0200

    autenticar y sesion

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var authorController=require('../controllers/author_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res) {
 });
 
 router.param('quizId', quizController.load);
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 router.get('/quizes' , quizController.index);
 router.get('/quizes/:quizId(\\d+)' , quizController.show);
@@ -29,5 +35,7 @@ router.delete('/quizes/:quizId(\\d+)',quizController.destroy);
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+
+
 
 module.exports = router;
